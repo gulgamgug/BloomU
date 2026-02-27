0e70f65435c33af9cbbaf88ea3c8f605e265bc5d
Gulgamgug <gulagaramguguru@gmail.com>
2026-02-28 05:56:59 +0700

    font: add inter font family

--- app/src/main/java/com/kelompok3/bloomu/ui/theme/Type.kt
+++ app/src/main/java/com/kelompok3/bloomu/ui/theme/Type.kt
@@ -1,34 +1,55 @@
 package com.kelompok3.bloomu.ui.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.ui.text.ExperimentalTextApi
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontVariation
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.kelompok3.bloomu.R
+
+// Definisi Inter Variable Font Family
+@OptIn(ExperimentalTextApi::class)
+val InterFontFamily = FontFamily(
+    Font(
+        resId = R.font.inter_variable,
+        variationSettings = FontVariation.Settings(
+            FontVariation.weight(400) // Default weight
+        )
+    ),
+    Font(
+        resId = R.font.inter_variable_italic,
+        style = FontStyle.Italic,
+        variationSettings = FontVariation.Settings(
+            FontVariation.weight(400)
+        )
+    )
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = InterFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+    ),
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+        fontFamily = InterFontFamily,
+        fontWeight = FontWeight.SemiBold,
         fontSize = 22.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = InterFontFamily,
         fontWeight = FontWeight.Medium,
         fontSize = 11.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
     )
-    */
 )
