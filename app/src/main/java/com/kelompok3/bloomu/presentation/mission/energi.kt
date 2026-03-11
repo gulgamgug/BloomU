78f973378ca612b823de57c211146acdc01b4ee5
revuriii99 <reviwilerii@gmail.com>
2026-03-11 17:28:43 +0700

    tambah energi.kt dan aset drawable misi

--- app/src/main/java/com/kelompok3/bloomu/presentation/mission/energi.kt
+++ app/src/main/java/com/kelompok3/bloomu/presentation/mission/energi.kt
@@ -1,2 +1,92 @@
 package com.kelompok3.bloomu.presentation.mission
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.blur
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.kelompok3.bloomu.R
+import com.kelompok3.bloomu.ui.theme.BloomUTheme
+import com.kelompok3.bloomu.ui.theme.InterFontFamily
+
+@Composable
+fun energi() {
+    val lightPurpleBg = Brush.verticalGradient(
+        colors = listOf(Color(0xFFFFFFFF), Color(0xFFE9E3FF))
+    )
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(lightPurpleBg)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ellipse_1),
+            contentDescription = "ellipse",
+            modifier = Modifier
+                .align(Alignment.TopCenter)
+                .size(1000.dp)
+                .offset(y = (-450).dp)
+                .rotate(0f)
+                .alpha(0.7f)
+                .blur(100.dp)
+        )
+
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(24.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Spacer(modifier = Modifier.height(60.dp))
+            Text(
+                text = "Misi",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color(0xFF4A4A4A),
+                fontFamily = InterFontFamily
+            )
+            Spacer(modifier = Modifier.height(32.dp))
+
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 8.dp),
+                shape = RoundedCornerShape(24.dp),
+            ) {
+                
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun energiPreview() {
+    BloomUTheme(dynamicColor = false) {
+        energi()
+    }
+}
